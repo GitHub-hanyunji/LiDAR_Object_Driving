731489911ba4e84eeb80c43b8c0ea0b22816ab14
HanYunJi <151121556+GitHub-hanyunji@users.noreply.github.com>
2025-12-22 11:51:47 +0900

    Add files via upload

---
+++ wsl/sllidar_ros2/src/main.cpp
@@ -0,0 +1,13 @@
+#include <sllidar_ros2/sllidar_ros2.hpp>
+
+
+int main(int argc, char * argv[])
+{
+    // ROS2 초기화
+    rclcpp::init(argc, argv);
+    // Node 실행 (spin)
+    // 무한대기
+    // 메시지 들어오면 콜백함수 호출
+    rclcpp::spin(std::make_shared<LiDARDetectNode>());
+    rclcpp::shutdown();  // ROS2 종료
+}
